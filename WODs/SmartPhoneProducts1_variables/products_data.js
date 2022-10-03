392017a342be99d60dbd41107ab3bc53d3403671
aamarcon <111392634+aamarcon@users.noreply.github.com>
2022-10-03 12:17:49 -1000

    wods revision

---
+++ WODs/SmartPhoneProducts1_variables/products_data.js
@@ -0,0 +1,21 @@
+console.log("Product variables loaded ... ");
+var name1 = "HTC";
+var price1 = 45.00;
+var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+var name2 = "Apple";
+var price2 = 85.00;
+var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+var name3 = "Nokia";
+var price3 = 135.00;
+var image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+var name4 = "Samsung";
+var price4 = 95.00;
+var image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+var name5 = "Blackberry";
+var price5 = 3.00;
+var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
