ba1d058f54dc2205520a9eefb9de33362c56155f
aamarcon <111392634+aamarcon@users.noreply.github.com>
2022-10-07 11:58:05 -1000

    lab 11 someparts

--- Lab11/ex1Algo.js
+++ Lab11/ex1Algo.js
@@ -1,11 +1,12 @@
-step1=13;
-step2=parseInt(step1/4);
-step3 = step2 + step1;
-step4 = 2; // Not Jan, so look at month before on table
-step6 = step4 + step3;
-step7 = step6 + 3;
-step8 = total of step 5 or 7;
-step9 = if 2000 or 1900 check;
-step10 = if leap check;
-step11 = no total8 - 1;
-step12 = 0;
+step1 = 74;
+step2 = parseint(74/4); // 18.5 only 18 goes to next step
+step3 = step2 + step3; // 18 + 74 = 92
+step4 = 0; //is the month January? no October/ lookup table September therfore number is 0;
+step6 = step3 + step4; // 92 + 0 = 92
+step7 = addDAy + step6; // 4 + 92 
+step8 = total // total is 96
+step9 = 1900; // is the year 2000 or 1900? answer 1974
+step10 = false; //is a leap year? 1974 not leap
+step11 = 96/7; // Divided by 7 using the reminder 5
+Result = Friday; // 5 is related to Friday in the table. 
+
