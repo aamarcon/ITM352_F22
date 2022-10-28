15fc4a2e20f792ea84ccaf41cfd6a1301937b20c
aamarcon <111392634+aamarcon@users.noreply.github.com>
2022-10-27 17:34:54 -1000

    LAB13Again

---
+++ Lab13/info_server_Ex22cc.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next();
+});
+app.get('/test', function(request, response, next){
+    response.send(request.method + 'to path# ' + request.path);
+
+});
+
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
