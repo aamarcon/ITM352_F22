26ca95d7fbdd7fefc3463779d21a01af7c58097b
aamarcon <111392634+aamarcon@users.noreply.github.com>
2022-10-29 09:37:21 -1000

    server.js added

---
+++ Marcondes_Alexandre_Assignment1/server.js
@@ -0,0 +1,83 @@
+//get express
+var express = require('express');
+// make a variable app became an express object. 
+var app = express();
+
+// use a folder static name public
+app.use(express.static(__dirname + '/public'));
+
+//use url 
+app.use(express.urlencoded({ extended: true }));
+
+//function to check if it is a positive integer. 
+function isNonNegativeInteger (queryString, returnErrors = false) {
+    errors = []; // assume no errors at first
+
+    if(Number(queryString) != queryString){
+       errors.push('Not a number!'); //Add errors to errors array
+    }
+    else {
+      if (queryString < 0) errors.push('Negative value!'); // Check if it is non-negative
+      if (parseInt(queryString) != queryString) errors.push('Not an integer!'); // Check that it is an integer
+    }// Check if string is a number value
+    
+    if (returnErrors) {
+        return errors;
+    } else if (errors.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+app.get('/test', function(request, response, next){
+    console.log(request.method + 'Not the star(*) but get ' + request.path);
+    next();
+});
+
+//App all to catch any wrong request. 
+app.all('*', function (request, response, next) {
+    console.log("Something Went Wrong!! NOT A POST!! CATCH ALL (*)");
+    next();
+});
+
+var products = require(__dirname + '/products_data.json');
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+app.get("/products_data.json", function (request, response, next) {
+   response.type('.js');
+   var products_str = `var products = ${JSON.stringify(products)};`;
+   response.send(products_str);
+});
+
+app.post("/process_form", function (request, response) {
+    var userQty = request.body['quantity_textbox'];
+
+    if (typeof userQty != 'undefined') {
+        if(isNonNegativeInteger(userQty)){
+
+            let brand = products[0]['name'];
+            let brand_price = products[0]['price'];
+
+            products[0].total_sold += Number(userQty);
+
+            response.redirect('receipt.html?quantity=' + userQty);
+        } else {
+            response.redirect('order_page.html?error=Invalid%20Quantity&quantity_textbox=' + userQty);
+        };
+    
+    } 
+    
+ });
+ 
+
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
+
+
+
+
+
+
+
