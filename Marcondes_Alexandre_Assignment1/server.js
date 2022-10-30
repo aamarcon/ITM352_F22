b621cb7e86bb8a881845175597b427d0c2150d2b
aamarcon <111392634+aamarcon@users.noreply.github.com>
2022-10-30 11:46:35 -1000

    CSS change

--- Marcondes_Alexandre_Assignment1/server.js
+++ Marcondes_Alexandre_Assignment1/server.js
@@ -31,16 +31,12 @@ function isNonNegativeInteger (queryString, returnErrors = false) {
 }
 
 
-app.get('/test', function(request, response, next){
+app.get('/display.html', function(request, response, next){
     console.log(request.method + 'Not the star(*) but get ' + request.path);
     next();
 });
 
-//App all to catch any wrong request. 
-app.all('*', function (request, response, next) {
-    console.log("Something Went Wrong!! NOT A POST!! CATCH ALL (*)");
-    next();
-});
+
 
 var products = require(__dirname + '/products_data.json');
 products.forEach( (prod,i) => {prod.total_sold = 0});
@@ -70,6 +66,12 @@ app.post("/process_form", function (request, response) {
     } 
     
  });
+
+ //App all to catch any wrong request. 
+app.all('*', function (request, response, next) {
+    console.log("Something Went Wrong!! NOT A POST!! CATCH ALL (*)");
+    next();
+});
  
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
